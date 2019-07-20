f3b436fb8a83e1a9fe4dca348ad382f25de6e8ee
Linh Nguyen <linh0404@hotmail.com>
2019-07-20 14:20:24 +1000

    linked response to search fields

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,41 @@
 //Change to up date article search variable based on html
 // var queryURL = url + startDate + sDate + endDate + eDate + search + articlesearch + apiKey;
 var apiKey = "EZAhwXGFlMNa2PfCAG2k0LQnwQZ77c9Y";
-var search = "coffee";
-var Yearstart = 1998;
+var search = "";
+var yearstart = "";
+var queryURL = "";
 // var endDate = YearEnd;
 // var search = Search;
 console.log(search);
-var queryURL =
-  "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-  search +
-  "&pub_year=" +
-  Yearstart +
-  "&api-key=" +
-  apiKey;
-// console.log(queryURL);
-var queryURL = $.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
-// Search
+
+$("#submit").on("click",function(e) {
+  e.preventDefault()
+
+  search = $("#search").val().trim();
+  yearstart = $("#yearstart").val().trim();
+
+  queryURL =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
+    search +
+    "&pub_year=" +
+    yearstart +
+    "&api-key=" +
+    apiKey;
+
+  console.log(queryURL)
+
+  // console.log(queryURL);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    console.log(queryURL);
+  });
+})
+
+
+// Searchi
 // Record
 // YearStart = pub_year
 // YearEnd =
